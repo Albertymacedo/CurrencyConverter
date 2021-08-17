3d6a720bf851f469e8f38a75257160a0623c8b89
Albertymacedo <alberty.macedo@acad.ifma.edu.br>
2021-08-17 10:32:42 -0300

    completo

---
+++ spring-cloud-mysql/src/main/java/com/praxia/springcloudmysql/model/Contact.java
@@ -0,0 +1,28 @@
+package com.praxia.springcloudmysql.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+    private String phone;
+
+}
