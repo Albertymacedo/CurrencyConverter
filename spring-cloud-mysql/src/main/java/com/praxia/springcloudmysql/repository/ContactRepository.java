1e862e33de867f92c6d7f14889ca2b0e092cbb74
Albertymacedo <74199456+Albertymacedo@users.noreply.github.com>
2021-11-04 17:48:54 -0300

    Delete spring-cloud-mysql

--- spring-cloud-mysql/src/main/java/com/praxia/springcloudmysql/repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.praxia.springcloudmysql.repository;
-
-import com.praxia.springcloudmysql.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<Contact, Long> { }
